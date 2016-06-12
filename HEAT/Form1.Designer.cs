cb0b384d948f56530b8caa618cac5e761f515be4
toan lam <lamtantoan996@gmail.com>
2016-06-12 20:33:37 +0700

    Lan Chat

---
+++ HEAT/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HEAT
+{
+    partial class frmHeat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.txtSendToUser = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SendToUser = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(354, 515);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(144, 46);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(165, 515);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(146, 46);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(165, 407);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(333, 79);
+            this.txtSend.TabIndex = 9;
+            this.txtSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSend_KeyDown);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.BackColor = System.Drawing.SystemColors.Control;
+            this.txtReceive.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtReceive.Location = new System.Drawing.Point(165, 153);
+            this.txtReceive.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ReadOnly = true;
+            this.txtReceive.Size = new System.Drawing.Size(333, 198);
+            this.txtReceive.TabIndex = 10;
+            // 
+            // txtSendToUser
+            // 
+            this.txtSendToUser.Location = new System.Drawing.Point(165, 67);
+            this.txtSendToUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSendToUser.Name = "txtSendToUser";
+            this.txtSendToUser.Size = new System.Drawing.Size(333, 25);
+            this.txtSendToUser.TabIndex = 11;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(165, 6);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(333, 25);
+            this.txtUsername.TabIndex = 8;
+            this.txtUsername.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyUp);
+            // 
+            // SendToUser
+            // 
+            this.SendToUser.AutoSize = true;
+            this.SendToUser.Location = new System.Drawing.Point(16, 70);
+            this.SendToUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SendToUser.Name = "SendToUser";
+            this.SendToUser.Size = new System.Drawing.Size(127, 18);
+            this.SendToUser.TabIndex = 5;
+            this.SendToUser.Text = "Send To Username : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 407);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Send Message : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 143);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Receive Message : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username : ";
+            // 
+            // frmHeat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 566);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.txtSendToUser);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.SendToUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmHeat";
+            this.Text = "HEAT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmHeat_FormClosing);
+            this.Load += new System.EventHandler(this.frmHeat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.TextBox txtSendToUser;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label SendToUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HEAT/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace HEAT
+{
+    public partial class frmHeat : Form
+    {
+        private Socket SendSock;
+        private Socket RecvSock;
+        private Thread thread;
+        byte[] bytes_username = new byte[4096];
+        byte[] bytes_content = new byte[4096];
+        byte[] bytes_sendtouser = new byte[4096];
+        string[] s = new string[3];
+        string content;
+        public frmHeat()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHeat_Load(object sender, EventArgs e)
+        {   
+            txtSendToUser.Enabled = false;
+            txtSend.Enabled = false;
+            txtReceive.Enabled = false;
+            RecvSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint IP = new IPEndPoint(IPAddress.Any,12345);
+            RecvSock.Bind(IP);
+            thread = new Thread(new ThreadStart(ThreadProcess));
+            thread.Start();
+        }
+        private void ThreadProcess()
+        {
+            while (true)
+            {
+                while (RecvSock.Available > 0)
+                {
+                    int NumByte = RecvSock.Receive(bytes_content);
+                    s[0] = Encoding.Unicode.GetString(bytes_content, 0, NumByte);
+
+                    NumByte = RecvSock.Receive(bytes_username);
+                    s[1] = Encoding.Unicode.GetString(bytes_username, 0, NumByte);
+
+                    NumByte = RecvSock.Receive(bytes_sendtouser);
+                    s[2] = Encoding.Unicode.GetString(bytes_sendtouser, 0, NumByte);
+
+                    if (s[2].Equals(txtUsername.Text))
+                    {
+                        txtReceive.Text += s[1] + " : " + s[0] + Environment.NewLine;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username này chưa connect !","Lỗi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        txtReceive.Text = "";
+                        return;
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint IP = new IPEndPoint(IPAddress.Broadcast,12345);
+            SendSock.Connect(IP);
+
+            content = txtSend.Text;
+            byte[] bytes_content = Encoding.Unicode.GetBytes(content);
+            SendSock.Send(bytes_content);
+
+            string username = txtUsername.Text;
+            byte[] bytes_username = Encoding.Unicode.GetBytes(username);
+            SendSock.Send(bytes_username);
+
+            string sendtouser = txtSendToUser.Text;
+            byte[] bytes_sendtouser = Encoding.Unicode.GetBytes(sendtouser);
+            SendSock.Send(bytes_sendtouser);
+
+            txtReceive.Text += txtUsername.Text + " : " + content + Environment.NewLine;
+            txtSend.Text = "";
+
+            SendSock.Close();
+        }
+
+        private void frmHeat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+            RecvSock.Close();
+        }
+
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                SendSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint IP = new IPEndPoint(IPAddress.Broadcast, 12345);
+                SendSock.Connect(IP);
+
+                content = txtSend.Text;
+                byte[] bytes_content = Encoding.Unicode.GetBytes(content);
+                SendSock.Send(bytes_content);
+
+                string username = txtUsername.Text;
+                byte[] bytes_username = Encoding.Unicode.GetBytes(username);
+                SendSock.Send(bytes_username);
+
+                string sendtouser = txtSendToUser.Text;
+                byte[] bytes_sendtouser = Encoding.Unicode.GetBytes(sendtouser);
+                SendSock.Send(bytes_sendtouser);
+
+                txtReceive.Text += txtUsername.Text + " : " + content + Environment.NewLine;
+                txtSend.Text = "";
+
+                SendSock.Close();
+            }
+        }
+
+        private void txtUsername_KeyUp(object sender, KeyEventArgs e)
+        {
+            content = txtUsername.Text;
+            txtSendToUser.Enabled = true;
+            txtSend.Enabled = true;
+            txtReceive.Enabled = true;
+            txtReceive.BackColor = Color.White;
+            if (content == "")
+            {
+                txtSendToUser.Enabled = false;
+                txtSend.Enabled = false;
+                txtReceive.Enabled = false;
+                txtReceive.BackColor = SystemColors.Control;
+
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            RecvSock.Close();
+            Close();
+        }
+
+
+
+    }
+}
